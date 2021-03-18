9253029d624f69ce44e7e0fe8ee12ad8bebf5aa0
SUHAIB KHAN <suhaib0900@gmail.com>
2021-03-18 15:53:54 +0530

    service

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 const AboutUs=()=>{
-    return <AboutSection/>;
+    return (
+        <>
+        <AboutSection/>
+        <ServicesSection/>
+        </>
+    )
 }
 export default AboutUs;
